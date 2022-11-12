913285a61dd1477b6670377038d182dda2488126
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-12 14:05:39 -0700

    completed the diagonalDifference exercise

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -1,16 +1,35 @@
 function diagonalDifference(arr) {
   // Write your code here
+  const reversedIndexes = [];
+  const sumA = [];
+  const sumB = [];
 
-  const sumA = arr[0][0] + arr[1][1] + arr[2][2];
-  const sumB = arr[0][2] + arr[1][1] + arr[2][0];
+  for (let index = 0; index < arr.length; index++) {
+    reversedIndexes.unshift(index);
+  }
 
-  return Math.abs(sumA - sumB);
+  for (let index = 0; index < arr.length; index++) {
+    sumA.push(arr[index][index]);
+    sumB.push(arr[index][reversedIndexes[index]]);
+  }
+
+  const arrayA = sumA.reduce((curr, acc) => curr + acc);
+  const arrayB = sumB.reduce((curr, acc) => curr + acc);
+
+  return Math.abs(arrayA - arrayB);
 }
+
+// console.log(
+//   diagonalDifference([
+//     [11, 2, 4],
+//     [4, 5, 6],
+//     [10, 8, -12],
+//   ])
+// );
 
 console.log(
   diagonalDifference([
-    [11, 2, 4],
-    [4, 5, 6],
-    [10, 8, -12],
+    [6, 8],
+    [-6, 9],
   ])
 );
