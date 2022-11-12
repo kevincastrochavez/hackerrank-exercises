f3b0026a232389773c5456264e7ff9000e43c624
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-12 13:22:40 -0700

    completed bigSum exercise

---
+++ bigSum.js
@@ -0,0 +1,8 @@
+function aVeryBigSum(ar) {
+  // Write your code here
+  return ar.reduce((curr, acc) => curr + acc);
+}
+
+console.log(
+  aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])
+);
