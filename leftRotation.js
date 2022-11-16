85409e812e3bf950197ac214113491fe3128fd9a
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-15 19:04:24 -0700

    solved the left rotation exercise

---
+++ leftRotation.js
@@ -0,0 +1,44 @@
+/*
+A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. Given an integer, , rotate the array that many steps left and return the result.
+
+Example
+
+
+After  rotations, .
+
+Function Description
+
+Complete the rotateLeft function in the editor below.
+
+rotateLeft has the following parameters:
+
+int d: the amount to rotate by
+int arr[n]: the array to rotate
+Returns
+
+int[n]: the rotated array
+Input Format
+
+The first line contains two space-separated integers that denote , the number of integers, and , the number of left rotations to perform.
+The second line contains  space-separated integers that describe .
+
+Constraints
+
+Sample Input
+1 2 3 4 5
+
+Sample Output
+5 1 2 3 4
+*/
+
+function rotateLeft(d, arr) {
+  // Write your code here
+  for (let index = 0; index < d; index++) {
+    const itemToMove = arr.shift();
+    arr.push(itemToMove);
+  }
+
+  return arr;
+}
+
+rotateLeft(2, [1, 2, 3, 4, 5]);
