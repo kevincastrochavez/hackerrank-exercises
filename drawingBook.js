c5e81cc22c4b0af380948631b1ffa589e92f950c
Kevin Castro <cas20057@byui.edu>
2024-04-10 12:27:09 -0600

    solved the drawing book problem

---
+++ drawingBook.js
@@ -0,0 +1,28 @@
+// A teacher asks the class to open their books to a page number. A student can either start turning pages from the front of the book or from the back of the book. They always turn pages one at a time. When they open the book, page  is always on the right side:
+
+// When they flip page , they see pages  and . Each page except the last page will always be printed on both sides. The last page may only be printed on the front, given the length of the book. If the book is  pages long, and a student wants to turn to page , what is the minimum number of pages to turn? They can start at the beginning or the end of the book.
+
+// Given  and , find and print the minimum number of pages that must be turned in order to arrive at page .
+
+// Example
+// Using the diagram above, if the student wants to get to page , they open the book to page , flip  page and they are on the correct page. If they open the book to the last page, page , they turn  page and are at the correct page. Return .
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n, p) {
+  const frontFlip = Math.floor(p / 2);
+  const backFlip = Math.floor(n / 2 - frontFlip);
+
+  const result = Math.min(frontFlip, backFlip);
+  return result;
+}
+
+pageCount(6, 2);
+pageCount(5, 4);
