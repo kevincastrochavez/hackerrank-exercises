266bc4019eec0a0b496a38b53edce7d252002354
Kevin Castro <cas20057@byui.edu>
2024-04-15 10:54:01 -0600

    solved the chunk challenge

---
+++ chunk.js
@@ -0,0 +1,26 @@
+// Lodash's Chunk method
+// Implement a function chunk(array, [size=1]) that splits the input array into groups of length size and returns them within a new array. If array can't be split evenly, the final chunk will be the remaining elements. The function should not modify the original input array.
+
+// Arguments
+// array (Array): The array to process.
+// [size=1] (number): The length of each chunk.
+// Returns
+// (Array): Returns the new array of chunks.
+
+/**
+ * @param {Array} array The array to process.
+ * @param {number} [size=1] The length of each chunk.
+ * @returns {Array<Array>} The new array of chunks.
+ */
+export default function chunk(array, size = 1) {
+  // length defines the new array's length and the size defines the length of each chunk
+  return Array.from({ length: Math.ceil(array.length / size) }, (_, index) => {
+    // mapFunction is used to create a new chunk based on the index
+    return array.slice(index * size, index * size + size);
+  });
+}
+
+// Examples
+chunk(['a', 'b', 'c', 'd']); // => [['a'], ['b'], ['c'], ['d']]
+chunk([1, 2, 3, 4], 2); // => [[1, 2], [3, 4]]
+chunk([1, 2, 3, 4], 3); // => [[1, 2, 3], [4]]
